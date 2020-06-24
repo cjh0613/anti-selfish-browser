7f5836d62f66d7122a6d1e7582f67bc84800e775
峡州仙士 <c@cjh0613.com>
2020-06-24 13:07:22 +0800

    first

---
+++ boycott-ie.js
@@ -0,0 +1,8 @@
+if (/*@cc_on!@*/false || (!!window.MSInputMethodContext && !!document.documentMode)){
+var lang = navigator.language||navigator.userLanguage;
+langtwo = lang.substr(0, 2);
+
+//Chinese
+if(langtwo == 'zh'){window.location.href="https://cjh0613.github.io/Boycott-IE/2/html/zh-Hans/please-upgrade-your-browser.html";}
+//else English
+ else{window.location.href="https://cjh0613.github.io/Boycott-IE/2/html/en/please-upgrade-your-browser.html";}}
