a7c1b6140f0689bf129b1495c13adbaaa0eeb600
峡州仙士 <c@cjh0613.com>
2020-06-27 15:06:41 +0800

    add languages

--- boycott-ie.js
+++ boycott-ie.js
@@ -3,6 +3,116 @@ var lang = navigator.language||navigator.userLanguage;
 langtwo = lang.substr(0, 2);
 
 //Chinese
-if(langtwo == 'zh'){window.location.href="https://cjh0613.github.io/Boycott-IE/2/html/zh-Hans/please-upgrade-your-browser.html";}
+if(langtwo == 'zh'){ if(lang == 'zh-Hant' || lang == 'zh-TW' || lang == 'zh-tw'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/zh-Hant/please-upgrade-your-browser.html";}
+ else{window.location.href="https://cjh0613.github.io/Boycott-IE/2/html/zh-Hans/please-upgrade-your-browser.html";}
+                   }
+//else
+else if(langtwo == 'af'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/af/please-upgrade-your-browser.html";}
+else if(langtwo == 'sq'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sq/please-upgrade-your-browser.html";}
+else if(langtwo == 'am'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/am/please-upgrade-your-browser.html";}
+else if(langtwo == 'ar'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ar/please-upgrade-your-browser.html";}
+else if(langtwo == 'hy'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/hy/please-upgrade-your-browser.html";}
+else if(langtwo == 'az'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/az/please-upgrade-your-browser.html";}
+else if(langtwo == 'eu'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/eu/please-upgrade-your-browser.html";}
+else if(langtwo == 'be'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/be/please-upgrade-your-browser.html";}
+else if(langtwo == 'bn'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/bn/please-upgrade-your-browser.html";}
+else if(langtwo == 'bs'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/bs/please-upgrade-your-browser.html";}
+else if(langtwo == 'bg'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/bg/please-upgrade-your-browser.html";}
+else if(langtwo == 'ca'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ca/please-upgrade-your-browser.html";}
+else if(langtwo == 'ce'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ceb/please-upgrade-your-browser.html";}
+else if(langtwo == 'co'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/co/please-upgrade-your-browser.html";}
+else if(langtwo == 'hr'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/hr/please-upgrade-your-browser.html";}
+else if(langtwo == 'cs'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/cs/please-upgrade-your-browser.html";}
+else if(langtwo == 'da'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/da/please-upgrade-your-browser.html";}
+else if(langtwo == 'nl'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/nl/please-upgrade-your-browser.html";}
+else if(langtwo == 'eo'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/eo/please-upgrade-your-browser.html";}
+else if(langtwo == 'et'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/et/please-upgrade-your-browser.html";}
+else if(langtwo == 'fi'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/fi/please-upgrade-your-browser.html";}
+else if(langtwo == 'fr'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/fr/please-upgrade-your-browser.html";}
+else if(langtwo == 'fy'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/fy/please-upgrade-your-browser.html";}
+else if(langtwo == 'gl'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/gl/please-upgrade-your-browser.html";}
+else if(langtwo == 'ka'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ka/please-upgrade-your-browser.html";}
+else if(langtwo == 'de'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/de/please-upgrade-your-browser.html";}
+else if(langtwo == 'el'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/el/please-upgrade-your-browser.html";}
+else if(langtwo == 'gu'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/gu/please-upgrade-your-browser.html";}
+else if(langtwo == 'ht'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ht/please-upgrade-your-browser.html";}
+else if(langtwo == 'ha'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ha/please-upgrade-your-browser.html";}
+else if(langtwo=='he' || langtwo=='iw'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/he/please-upgrade-your-browser.html";}
+else if(langtwo == 'hi'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/hi/please-upgrade-your-browser.html";}
+else if(langtwo == 'hm'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/hmn/please-upgrade-your-browser.html";}
+else if(langtwo == 'hu'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/hu/please-upgrade-your-browser.html";}
+else if(langtwo == 'is'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/is/please-upgrade-your-browser.html";}
+else if(langtwo == 'ig'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ig/please-upgrade-your-browser.html";}
+else if(langtwo == 'id'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/id/please-upgrade-your-browser.html";}
+else if(langtwo == 'ga'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ga/please-upgrade-your-browser.html";}
+else if(langtwo == 'it'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/it/please-upgrade-your-browser.html";}
+else if(langtwo == 'ja'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ja/please-upgrade-your-browser.html";}
+else if(langtwo == 'jv'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/jv/please-upgrade-your-browser.html";}
+else if(langtwo == 'kn'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/kn/please-upgrade-your-browser.html";}
+else if(langtwo == 'kk'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/kk/please-upgrade-your-browser.html";}
+else if(langtwo == 'km'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/km/please-upgrade-your-browser.html";}
+else if(langtwo == 'rw'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/rw/please-upgrade-your-browser.html";}
+else if(langtwo == 'ko'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ko/please-upgrade-your-browser.html";}
+else if(langtwo == 'ku'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ku/please-upgrade-your-browser.html";}
+else if(langtwo == 'ky'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ky/please-upgrade-your-browser.html";}
+else if(langtwo == 'lo'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/lo/please-upgrade-your-browser.html";}
+else if(langtwo == 'la'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/la/please-upgrade-your-browser.html";}
+else if(langtwo == 'lv'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/lv/please-upgrade-your-browser.html";}
+else if(langtwo == 'lt'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/lt/please-upgrade-your-browser.html";}
+else if(langtwo == 'lb'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/lb/please-upgrade-your-browser.html";}
+else if(langtwo == 'mk'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/mk/please-upgrade-your-browser.html";}
+else if(langtwo == 'mg'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/mg/please-upgrade-your-browser.html";}
+else if(langtwo == 'ms'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ms/please-upgrade-your-browser.html";}
+else if(langtwo == 'ml'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ml/please-upgrade-your-browser.html";}
+else if(langtwo == 'mt'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/mt/please-upgrade-your-browser.html";}
+else if(langtwo == 'mi'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/mi/please-upgrade-your-browser.html";}
+else if(langtwo == 'mr'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/mr/please-upgrade-your-browser.html";}
+else if(langtwo == 'mn'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/mn/please-upgrade-your-browser.html";}
+else if(langtwo == 'my'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/my/please-upgrade-your-browser.html";}
+else if(langtwo == 'ne'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ne/please-upgrade-your-browser.html";}
+else if(langtwo == 'no'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/no/please-upgrade-your-browser.html";}
+else if(langtwo == 'ny'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ny/please-upgrade-your-browser.html";}
+else if(langtwo == 'or'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/or/please-upgrade-your-browser.html";}
+else if(langtwo == 'ps'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ps/please-upgrade-your-browser.html";}
+else if(langtwo == 'fa'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/fa/please-upgrade-your-browser.html";}
+else if(langtwo == 'pl'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/pl/please-upgrade-your-browser.html";}
+else if(langtwo == 'pt'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/pt/please-upgrade-your-browser.html";}
+else if(langtwo == 'pa'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/pa/please-upgrade-your-browser.html";}
+else if(langtwo == 'ro'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ro/please-upgrade-your-browser.html";}
+else if(langtwo == 'ru'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ru/please-upgrade-your-browser.html";}
+else if(langtwo == 'sm'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sm/please-upgrade-your-browser.html";}
+else if(langtwo == 'gd'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/gd/please-upgrade-your-browser.html";}
+else if(langtwo == 'sr'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sr/please-upgrade-your-browser.html";}
+else if(langtwo == 'st'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/st/please-upgrade-your-browser.html";}
+else if(langtwo == 'sn'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sn/please-upgrade-your-browser.html";}
+else if(langtwo == 'sd'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sd/please-upgrade-your-browser.html";}
+else if(langtwo == 'si'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/si/please-upgrade-your-browser.html";}
+else if(langtwo == 'sk'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sk/please-upgrade-your-browser.html";}
+else if(langtwo == 'sl'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sl/please-upgrade-your-browser.html";}
+else if(langtwo == 'so'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/so/please-upgrade-your-browser.html";}
+else if(langtwo == 'es'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/es/please-upgrade-your-browser.html";}
+else if(langtwo == 'su'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/su/please-upgrade-your-browser.html";}
+else if(langtwo == 'sw'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sw/please-upgrade-your-browser.html";}
+else if(langtwo == 'sv'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/sv/please-upgrade-your-browser.html";}
+else if(langtwo == 'tl'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/tl/please-upgrade-your-browser.html";}
+else if(langtwo == 'tg'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/tg/please-upgrade-your-browser.html";}
+else if(langtwo == 'ta'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ta/please-upgrade-your-browser.html";}
+else if(langtwo == 'tt'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/tt/please-upgrade-your-browser.html";}
+else if(langtwo == 'te'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/te/please-upgrade-your-browser.html";}
+else if(langtwo == 'th'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/th/please-upgrade-your-browser.html";}
+else if(langtwo == 'tr'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/tr/please-upgrade-your-browser.html";}
+else if(langtwo == 'tk'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/tk/please-upgrade-your-browser.html";}
+else if(langtwo == 'uk'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/uk/please-upgrade-your-browser.html";}
+else if(langtwo == 'ur'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ur/please-upgrade-your-browser.html";}
+else if(langtwo == 'ug'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/ug/please-upgrade-your-browser.html";}
+else if(langtwo == 'uz'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/uz/please-upgrade-your-browser.html";}
+else if(langtwo == 'vi'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/vi/please-upgrade-your-browser.html";}
+else if(langtwo == 'cy'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/cy/please-upgrade-your-browser.html";}
+else if(langtwo == 'xh'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/xh/please-upgrade-your-browser.html";}
+else if(langtwo == 'yi'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/yi/please-upgrade-your-browser.html";}
+else if(langtwo == 'yo'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/yo/please-upgrade-your-browser.html";}
+else if(langtwo == 'zu'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/zu/please-upgrade-your-browser.html";}
+ 
+else if(lang == 'haw'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/haw/please-upgrade-your-browser.html";}
 //else English
- else{window.location.href="https://cjh0613.github.io/Boycott-IE/2/html/en/please-upgrade-your-browser.html";}}
+ else{window.location.href="https://boycott-ie.github.io/boycott-ie/html/en/please-upgrade-your-browser.html";}}
